b92ba0a082052d82956fde978ce4e0bcaa45d050
HammadSiddique <hammad.siddique01@gmail.com>
2022-05-24 20:44:12 +0500

    Render recipe cards with API

---
+++ src/modules/displayItems.js
@@ -0,0 +1,25 @@
+const displayCards = () => {
+  const recipeCards = document.querySelector('.recipe-cards');
+  fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian')
+    .then((response) => response.json())
+    .then((data) => {
+      let html = '';
+      data.meals.forEach((meal) => {
+        html += `<div class="card" id="${meal.idMeal}">
+               <div class="card-img">
+                   <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+               </div>
+               <div class="card-info">
+                   <h2>${meal.strMeal}</h2>
+                   <ul>
+                       <li><button class="comment-btn" type="button">Comments</button></li>
+                       <li><button class="like-btn"><i class="fa-regular fa-heart"></i><span class="like-count">15</span></button></li>
+                   </ul>
+               </div>
+             </div>`;
+        recipeCards.innerHTML = html;
+      });
+    });
+};
+
+export default displayCards;
