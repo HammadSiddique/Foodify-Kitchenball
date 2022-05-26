0b70ff647c8df5c6ca958820befaf06c13a8f7d1
unknown <ibr.s.ahmat@gmail.com>
2022-05-27 00:41:09 +0300

    Add mobile menu

--- src/index.js
+++ src/index.js
@@ -1,24 +1,45 @@
 import './index.css';
 import displayCards from './modules/displayItems.js';
 
+const mainMenu = document.querySelector('.nav-items');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .nav-items li a');
+
 const contactBtn = document.querySelector('#contactSection');
 const recipesSection = document.querySelector('#recipesSection');
 
 const recipes = document.querySelector('#recipe-section');
 const contact = document.querySelector('#contact');
 
+
+
+openMenu.addEventListener('click', () => {
+    mainMenu.style = 'display: flex; top: 0;';
+});
+
+closeMenu.addEventListener('click', () => {
+    mainMenu.style = 'top:-100%';
+});
+
+menu_items.forEach(item => {
+    item.addEventListener('click', () => {
+        mainMenu.style = 'top:-100%';
+    });
+});
+
 contactBtn.addEventListener('click', () => {
-  recipes.classList.add('hidden');
-  contact.classList.remove('hidden');
+    recipes.classList.add('hidden');
+    contact.classList.remove('hidden');
 });
 
 recipesSection.addEventListener('click', () => {
-  contact.classList.add('hidden');
-  recipes.classList.remove('hidden');
+    contact.classList.add('hidden');
+    recipes.classList.remove('hidden');
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-  displayCards();
-  contact.classList.add('hidden');
-  recipes.classList.remove('hidden');
+    displayCards();
+    contact.classList.add('hidden');
+    recipes.classList.remove('hidden');
 });
