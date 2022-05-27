db5fa54c0bf1d9d6810fcb617779cb7ca16fe362
HammadSiddique <hammad.siddique01@gmail.com>
2022-05-27 17:50:05 +0500

    Create mobile menu

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const recipesSection = document.querySelector('#recipesSection');
 const recipes = document.querySelector('#recipe-section');
 const contact = document.querySelector('#contact');
 
+const mainMenu = document.querySelector('.nav-items');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menuItems = document.querySelectorAll('nav .nav-items li');
+
 contactBtn.addEventListener('click', () => {
   recipes.classList.add('hidden');
   contact.classList.remove('hidden');
@@ -15,6 +20,20 @@ contactBtn.addEventListener('click', () => {
 recipesSection.addEventListener('click', () => {
   contact.classList.add('hidden');
   recipes.classList.remove('hidden');
+});
+
+openMenu.addEventListener('click', () => {
+  mainMenu.style = 'display: flex; top:0;';
+});
+
+closeMenu.addEventListener('click', () => {
+  mainMenu.style = 'display: none;';
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    mainMenu.style = 'display: ';
+  });
 });
 
 window.addEventListener('DOMContentLoaded', () => {
