d9c62716a816bf8ab4449f5f572f73edb258b439
Hammad Siddique <hammad.siddique01@gmail.com>
2022-05-27 12:04:16 +0500

    Revert "Foodify Kitchenball: Mobile menu"

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import './index.css';
 import displayCards from './modules/displayItems.js';
 
-const mainMenu = document.querySelector('.nav-items');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const menuItems = document.querySelectorAll('nav .nav-items li a');
-
 const contactBtn = document.querySelector('#contactSection');
 const recipesSection = document.querySelector('#recipesSection');
 
 const recipes = document.querySelector('#recipe-section');
 const contact = document.querySelector('#contact');
-
-openMenu.addEventListener('click', () => {
-  mainMenu.style = 'display: flex; top: 0;';
-});
-
-closeMenu.addEventListener('click', () => {
-  mainMenu.style = 'top:-100%';
-});
-
-menuItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    mainMenu.style = 'top:-100%';
-  });
-});
 
 contactBtn.addEventListener('click', () => {
   recipes.classList.add('hidden');
